1569eb971d9e009047520eb52c92a9db01fba9e5
elllipse <elllipse@mail.ru>
2016-07-14 18:05:43 +0300

    wrap into document.ready function

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,31 +1,33 @@
 (function($) {
 	"use strict";
+	$(document).ready(function() {
 
-	//dom elements
-	var $homeLinks = $("a[href^='#']"),
+		//dom elements
+		var $homeLinks = $("a[href^='#']"),
 		$body = $("html, body");
 
-	//settings	
-	var settings = {
+		//settings	
+		var settings = {
 			navOffset : 55,
 			scrollTime : 1000
 		};
 
-	//events
-	$homeLinks.on("click", clickOnHomeLinks);
+		//events
+		$homeLinks.on("click", clickOnHomeLinks);
 
-	function moveToTarget(pos) {
-		$body.stop().animate({scrollTop : pos - settings.navOffset}, settings.scrollTime);
-	};
+		function moveToTarget(pos) {
+			$body.stop().animate({scrollTop : pos - settings.navOffset}, settings.scrollTime);
+		};
 
-	//functions
-	function clickOnHomeLinks(e) {
-		var $this = $(this),
+		//functions
+		function clickOnHomeLinks(e) {
+			var $this = $(this),
 			target = $this.attr("href"),
 			targetPosition = $(target).offset().top;
 
-		e.preventDefault();
-		moveToTarget(targetPosition);
-	};
+			e.preventDefault();
+			moveToTarget(targetPosition);
+		};
 
+	});
 })(jQuery);

1bdb5a001fe208f9aa8a4dca4760e0b9b72415ff
elllipse <elllipse@mail.ru>
2016-07-14 16:41:39 +0300

    split scroll function into components

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,18 +1,22 @@
 (function($) {
 
 	var $homeLinks = $("a[href^='#']");
+	var $body = $("html, body");
 	var navOffset = 55;
 	$homeLinks.on("click", clickOnHomeLinks);
 
 
+	function moveToTarget(pos) {
+		$body.stop().animate({scrollTop : pos - navOffset}, 1000);
+	}
+
 	function clickOnHomeLinks(e) {
-		var $this = $(this);
-		var targetToMove = $this.attr("href");
-		var moveTo = $(targetToMove).offset().top;
+		var $this = $(this),
+			target = $this.attr("href"),
+			targetPosition = $(target).offset().top;
 
 		e.preventDefault();
-		var $body = $("html, body");
-		$body.stop().animate({scrollTop : moveTo - navOffset}, 1000)
+		moveToTarget(targetPosition);
 	}
 
 })(jQuery);
